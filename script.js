e776dd97ad61f36e5a470cb85a10e56baf512b7d
Jayden <jojugeorgemathew@gmail.com>
2026-08-15 23:28:23 +0530

    Add 'Expense Tracker' app to the application list
    Updated app list with new 'Expense Tracker' app and modified descriptions.

--- script.js
+++ script.js
@@ -2,11 +2,7 @@
 const apps = [
 
 	{id:'1',title:'Workflow Management',desc:'Create Workflows & flowcharts',tag:'Utility',url:'https://apps.aiwithjoju.com/workflows/'},
-	{id:'2',title:'Color Mixer',desc:'Blend colors and export palettes',tag:'Design',url:'#'},
-	{id:'3',title:'JSON Formatter',desc:'Pretty-print and validate JSON',tag:'Utility',url:'#'},
-	{id:'4',title:'Todo Lite',desc:'Minimal, fast todo app',tag:'Productivity',url:'#'},
-	{id:'5',title:'Image Optimizer',desc:'Resize and compress images',tag:'Media',url:'#'},
-	{id:'6',title:'Unit Converter',desc:'Convert units quickly',tag:'Utility',url:'#'}
+	{id:'2',title:'Expense Tracker',desc:'Track your daily expenses',tag:'Productivity',url:'https://apps.aiwithjoju.com/expense-tracker/'},
 
 ];
 
